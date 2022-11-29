e2a54dfb673b7259248a4086b12277ee5b86472f
Sergey Makarichev <harleymack@mail.ru>
2022-11-29 20:56:16 +0300

    5 more 6-kyu tasks done

--- src/main/kotlin/codewars/PracticeKyu6.kt
+++ src/main/kotlin/codewars/PracticeKyu6.kt
@@ -1,6 +1,8 @@
 package codewars
 
 import java.lang.ArithmeticException
+import java.util.*
+import javax.swing.Box
 import kotlin.math.atan2
 import kotlin.math.ln
 import kotlin.math.sqrt
@@ -16,9 +18,11 @@ class PracticeKyu6 {
     fun unless(cond: Boolean, func: () -> Unit) = if (!cond) func() else Unit
     fun until(cond: () -> Boolean, func: () -> Unit) =
         run { while (!cond()) func() }
+
     fun forceRun(func: () -> Unit) = try {
         func()
-    } catch (_: Exception) {}
+    } catch (_: Exception) {
+    }
 
     var concatString = String::plus
 
@@ -33,5 +37,62 @@ class PracticeKyu6 {
         )
     }
 
-}
+    fun loop(random: Random, int: Int): Int {
+        repeat(int) {
+            random.nextInt()
+        }
+        return random.nextInt()
+    }
 
+    operator fun Box.invoke(x: Box.() -> Unit) = x(this)
+
+    fun decomposeDoubleStrand(doubleStrand: String): String {
+        val reverseSeq = doubleStrand.map {
+            when (it) {
+                'A' -> 'T'
+                'G' -> 'C'
+                'T' -> 'A'
+                'C' -> 'G'
+                else -> '0'
+            }
+        }.reversed().joinToString("")
+
+        val f1 = "Frame 1: ${doubleStrand.chunked(3).joinToString(" ")}"
+        val f2 = "Frame 2: ${doubleStrand.take(1)} ${
+            doubleStrand.drop(1).chunked(3).joinToString(" ")
+        }"
+        val f3 = "Frame 3: ${doubleStrand.take(2)} ${
+            doubleStrand.drop(2).chunked(3).joinToString(" ")
+        }"
+        val rf1 = "Reverse Frame 1: ${
+            reverseSeq.chunked(3).joinToString(" ")
+        }"
+        val rf2 = "Reverse Frame 2: ${reverseSeq.take(1)} ${
+            reverseSeq.drop(1).chunked(3).joinToString(" ")
+        }"
+        val rf3 = "Reverse Frame 3: ${reverseSeq.take(2)} ${
+            reverseSeq.drop(2).chunked(3).joinToString(" ")
+        }"
+        return "$f1\n$f2\n$f3\n\n$rf1\n$rf2\n$rf3"
+    }
+
+    fun decode(input: String?): String {
+        return input!!
+            .split("\\u")
+            .filter { it.isNotEmpty() }
+            .map { it.toInt(16).toChar() }
+            .joinToString("")
+    }
+
+    fun encode(input: String?): String {
+        return input!!
+            .map { "\\u%04x".format(it.code) }
+            .joinToString("")
+    }
+
+    fun extractFileName(dirtyFileName: String): String {
+        return dirtyFileName
+            .substringAfter("_")
+            .substringBeforeLast(".")
+    }
+}
