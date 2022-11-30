8094c55e330dd62577d521753c6d790b4a5e6e94
Sergey Makarichev <harleymack@mail.ru>
2022-11-30 19:06:36 +0300

    5 more 6-kyu tasks done

--- src/main/kotlin/codewars/PracticeKyu6.kt
+++ src/main/kotlin/codewars/PracticeKyu6.kt
@@ -95,4 +95,70 @@ class PracticeKyu6 {
             .substringAfter("_")
             .substringBeforeLast(".")
     }
+
+    fun jumping(arr: IntArray): Boolean {
+        var minJump = arr.size - 1
+        for (i in minJump - 1 downTo 0) {
+            if (i + arr[i] >= minJump)
+                minJump = i
+        }
+        return minJump == 0
+    }
+
+    fun findLast(n: Int, m: Int): IntArray {
+        var firstPos = (m - 1) % n
+        val players = (1..n).toMutableList()
+
+        while (players.size > 1) {
+            players.removeAt(firstPos)
+            firstPos = (firstPos + m - 1) % players.size
+        }
+
+        return intArrayOf(
+            players.first(),
+            (n + 1) * (n - m).coerceAtLeast(0) +
+                    m * (m - 1).coerceAtMost(n - 1)
+        )
+    }
+
+    fun numberAndIPaddress(s: String): String =
+        if (s.contains("."))
+            s.split(".").joinToString("") {
+                it.toInt().toString(2).padStart(8, '0')
+            }
+                .toLong(2).toString()
+        else
+            s.toLong().toString(2)
+                .padStart(32, '0').chunked(8)
+                .joinToString(".") { "${it.toInt(2)}" }
+
+    fun winChecker(board: String): Boolean {
+        val rgxFirst =
+            "XXX......|...XXX...|......XXX|X..X..X|X...X...X|..X.X.X..".toRegex()
+        val rgxSecond =
+            "OOO......|...OOO...|......OOO|O..O..O|O...O...O|..O.O.O..".toRegex()
+        return rgxFirst.containsMatchIn(board) || rgxSecond.containsMatchIn(board)
+    }
+
+}
+
+class Leetspeak : Encoder() {
+
+    override fun encode(source: String?): String {
+        return (source ?: "").map {
+            when (it.toLowerCase()) {
+                'a' -> "4"
+                'e' -> "3"
+                'l' -> "1"
+                'm' -> "/^^\\"
+                'o' -> "0"
+                'u' -> "(_)"
+                else -> it
+            }
+        }.joinToString("")
+    }
+}
+
+abstract class Encoder {
+    abstract fun encode(source: String?): String
 }
